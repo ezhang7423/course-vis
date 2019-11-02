99f2302c9dd7b68457e69d2d0b7a5e863b0691ac
ezhang7423 <ezipe@workhorse.localdomain>
2019-11-02 10:52:09 -0700

    real data

--- data.js
+++
@@ -1,7 +0,0 @@
-const data = [
-  {"text":"Math 4A","description":"bigelow is cool"},
-  {"text":"Math 4B","description":"blabla"},
-  {"text":"Math 6A","description":"bobo"},
-  {"text":"Math 6B","description":"foobar"},
-];
-const pair = [[0,2], [1,2]];
---
+++ datasource.js
@@ -0,0 +1,102 @@
+const courses = [
+    {
+        "text": "2A",
+        "description": "Calculus with Algebra and Trigonometry"
+    },
+    {
+        "text": "2B",
+        "description": "Calculus with Algebra and Trigonometry"
+    },
+    {
+        "text": "4A",
+        "description": "Linear Algebra with Applications"
+    },
+    {
+        "text": "4B",
+        "description": "Differential Equations"
+    },
+    {
+        "text": "6A",
+        "description": "Vector Calculus with Applications, First Course"
+    },
+    {
+        "text": "6B",
+        "description": "Vector Calculus with Applications, Second Course"
+    },
+    {
+        "text": "8",
+        "description": "Transition to Higher Mathematics",
+        "optionalPrerequistes": [
+            "4A",
+            "4B",
+            "6A"
+        ]
+    },
+    {
+        "text": "108A",
+        "description": "Introduction to Linear Algebra"
+    },
+    {
+        "text": "108B",
+        "description": "Introduction to Linear Algebra"
+    },
+    {
+        "text": "111A",
+        "description": "Introduction to Abstract Algebra"
+    },
+    {
+        "text": "111B",
+        "description": "Abstract Algebra"
+    }
+]
+
+const valuePairs = [
+    [
+        "2A",
+        "2B"
+    ],
+    [
+        "2B",
+        "4A"
+    ],
+    [
+        "4A",
+        "4B"
+    ],
+    [
+        "4A",
+        "6A"
+    ],
+    [
+        "4A",
+        "8"
+    ],
+    [
+        "4A",
+        "108A"
+    ],
+    [
+        "6A",
+        "6B"
+    ],
+    [
+        "4B",
+        "8"
+    ],
+    [
+        "8",
+        "108A"
+    ],
+    [
+        "108A",
+        "108B"
+    ],
+    [
+        "108A",
+        "111A"
+    ],
+    [
+        "111A",
+        "111B"
+    ]
+]
