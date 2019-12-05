7331b42f873d77b49c4c0b50df994cd15fbcd061
Eddie Zhang <ezhang7423@student.palomar.edu>
2019-12-04 22:45:52 -0800

    cs data

---
+++ data/csMajor.js
@@ -0,0 +1,285 @@
+const courses = [
+    {
+        "text": "8",
+        "description": "Introduction to Computer Science"
+    },
+    {
+        "text": "16",
+        "description": "fill l8r"
+    },
+    {
+        "text": "24",
+        "description": "fill l8r"
+    },
+    {
+        "text": "32",
+        "description": "fill l8r"
+    },
+    {
+        "text": "40",
+        "description": "fill l8r"
+    },
+    {
+        "text": "48",
+        "description": "fill l8r"
+    },
+    {
+        "text": "48",
+        "description": "fill l8r"
+    },
+    {
+        "text": "56",
+        "description": "fill l8r"
+    },
+    {
+        "text": "64",
+        "description": "fill l8r"
+    },
+    {
+        "text": "111",
+        "description": "fill l8r"
+    },
+    {
+        "text": "140",
+        "description": "fill l8r"
+    },
+    {
+        "text": "130A",
+        "description": "fill l8r"
+    },
+    {
+        "text": "130B",
+        "description": "fill l8r"
+    },
+    {
+        "text": "138",
+        "description": "fill l8r"
+    },
+    {
+        "text": "154",
+        "description": "fill l8r"
+    },
+    {
+        "text": "160",
+        "description": "fill l8r"
+    },
+    {
+        "text": "162",
+        "description": "fill l8r"
+    },
+    {
+        "text": "170",
+        "description": "fill l8r"
+    },
+    {
+        "text": "171",
+        "description": "fill l8r"
+    },
+    {
+        "text": "174A",
+        "description": "fill l8r"
+    },
+    {
+        "text": "174B",
+        "description": "fill l8r"
+    },
+    {
+        "text": "176A",
+        "description": "fill l8r"
+    },
+    {
+        "text": "176B",
+        "description": "fill l8r"
+    },
+    {
+        "text": "176C",
+        "description": "fill l8r"
+    },
+    {
+        "text": "177",
+        "description": "fill l8r"
+    },
+    {
+        "text": "178",
+        "description": "fill l8r"
+    },
+    {
+        "text": "180",
+        "description": "fill l8r"
+    },
+    {
+        "text": "184",
+        "description": "fill l8r"
+    },
+    {
+        "text": "185",
+        "description": "fill l8r"
+    },
+    {
+        "text": "189A",
+        "description": "fill l8r"
+    },
+    {
+        "text": "189B",
+        "description": "fill l8r"
+    },
+    {
+        "text": "190AA-ZZ",
+        "description": "fill l8r"
+    },
+    {
+        "text": "192",
+        "description": "fill l8r"
+    },
+    {
+        "text": "196",
+        "description": "fill l8r"
+    },
+    {
+        "text": "196B",
+        "description": "fill l8r"
+    }
+]
+
+const valuePairs = [
+    [
+        "16",
+        "24"
+    ],
+    [
+        "24",
+        "32"
+    ],
+    [
+        "32",
+        "130A"
+    ],
+    [
+        "16",
+        "40"
+    ],
+    [
+        "40",
+        "130A"
+    ],
+    [
+        "32",
+        "48"
+    ],
+    [
+        "32",
+        "56"
+    ],
+    [
+        "16",
+        "64"
+    ],
+    [
+        "24",
+        "111"
+    ],
+    [
+        "130A",
+        "130B"
+    ],
+    [
+        "40",
+        "138"
+    ],
+    [
+        "130A",
+        "140"
+    ],
+    [
+        "32",
+        "154"
+    ],
+    [
+        "48",
+        "154"
+    ],
+    [
+        "64",
+        "154"
+    ],
+    [
+        "64",
+        "160"
+    ],
+    [
+        "130A",
+        "160"
+    ],
+    [
+        "130A",
+        "162"
+    ],
+    [
+        "138",
+        "162"
+    ],
+    [
+        "130A",
+        "165A"
+    ],
+    [
+        "130A",
+        "165B"
+    ],
+    [
+        "130A",
+        "170"
+    ],
+    [
+        "154",
+        "170"
+    ],
+    [
+        "130A",
+        "171"
+    ],
+    [
+        "130A",
+        "174A"
+    ],
+    [
+        "32",
+        "176A"
+    ],
+    [
+        "176A",
+        "176B"
+    ],
+    [
+        "176B",
+        "176C"
+    ],   
+    [
+        "170",
+        "177"
+    ],    
+    [
+        "24",
+        "178"
+    ],    
+    [
+        "40",
+        "178"
+    ],  
+    [
+        "130A",
+        "180"
+    ],
+    [
+        "56",
+        "184"
+    ],
+    [
+        "56",
+        "189A"
+    ],
+    [
+        "189A",
+        "189B"
+    ],
+]
