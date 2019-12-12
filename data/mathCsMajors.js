d885d7dd2c3894602e640cf1961c1079415508e0
Eddie Zhang <54998055+ezhang7423@users.noreply.github.com>
2019-12-11 18:53:03 -0800

    add other classe

--- data/mathCsMajors.js
+++ data/mathCsMajors.js
@@ -27,5 +27,20 @@ let outsideCS = {
         units: '4',
         prerequisites: 'Mathematics 4A (or 4AI) or Mathematics 5A (or 5AI) with a letter grade of Cor better.',
         description: 'Calculus of functions of several variables, vector-valued functions of one variable, scalar and vector fields, integration along paths, double and triple integrals, integration over surfaces, properties, and applications of integrals, and classical integration theorems of vector calculus.'
+    },
+    "PSTAT 120A. Probability and Statistics": {
+        units: "4",
+        Prerequisite: "Math 3C or Math 4A or Math 4AI or Math 6A or Math 6AI completed with a minimum grade of C or better.",
+        description: "Concepts of probability; random variables; combinatorial probability; discrete and continuous distributions; joint distributions, expected values; moment generating functions; law of large numbers and central limit theorems."
+    },
+    "PSTAT 120B. Probability and Statistics": {
+        units: "4",
+        prerequisite: "PSTAT 120A with a grade of C or better.",
+        description: 'Distribution of sample mean and sample variance; t, chi-squared and F distributions; summarizing data by statistics and graphs; estimation theory for single samples: sufficiency, efficiency, consistency, method of moments, maximum likelihood; hypothesis testing: likelihood ratio test; confidence intervals.'
+    },
+    "ENGR 101: Ethics in Engineering": {
+        units: "3",
+        description: "The nature of moral value, normative judgment and moral reasoning. Theories of moral value. The engineer's role in society. Ethics in professional practice. Safety, risk, responsibility. Morality and career choice. Code of ethics. Case studies facilitate the comprehension of the concepts introduced.",
+        prerequisites: "PreRequisite Comments:	Senior standing in engineering. . See 'PreRequisite' tab"
     }
 }
